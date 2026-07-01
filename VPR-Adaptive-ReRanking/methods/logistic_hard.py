e0196fc0d314961e359eb83b43469e884fc5e8c7
tommasopantano01 <s349341@studenti.polito.it>
2026-07-01 02:53:56 +0200

    Update logistic_hard.py

--- VPR-Adaptive-ReRanking/methods/logistic_hard.py
+++ VPR-Adaptive-ReRanking/methods/logistic_hard.py
@@ -30,8 +30,6 @@ def parse_args():
     parser.add_argument("--im-size",    type=int, default=512)
     parser.add_argument("--num-preds",  type=int, default=20)
     parser.add_argument("--output-dir", required=True)
-    parser.add_argument("--model", required=True)
-    parser.add_argument("--z_data-dir", required=True)
     return parser.parse_args()
 
 
