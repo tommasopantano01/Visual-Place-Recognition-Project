58c51c56c4b5a05535bb07a39fb0943b9c9dca4c
tommasopantano01 <s349341@studenti.polito.it>
2026-09-01 11:37:23 +0200

    Implement ResNet-50 backbone for MixVPR model

--- VPR-methods-evaluation/vpr_models/mixvpr.py
+++ VPR-methods-evaluation/vpr_models/mixvpr.py
@@ -90,9 +90,30 @@ class MixVPR(nn.Module):
         x = F.normalize(x.flatten(1), p=2, dim=-1)
         return x
 
-### Implement ResNet-50 here for MixVPR model,
-### otherwise `self.backbone = ResNet()` will fail
-### (academic purpose)
+class ResNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.model = torchvision.models.resnet50(weights=None)
+
+        # MixVPR usa la feature map di layer3.
+        self.model.avgpool = nn.Identity()
+        self.model.fc = nn.Identity()
+        self.model.layer4 = nn.Identity()
+
+        self.out_channels = 1024
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+
+        return x
 
 
 class MixVPRModel(torch.nn.Module):
